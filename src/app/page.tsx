3e22b484163aada2dbaef69325973be65c03b9be
japhethbear <japhethbear@gmail.com>
2024-04-24 18:56:54 -0400

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,285 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
+import TheBearEssentialsGraphic from "@/assets/TheBearEssentialsGraphic.png";
+import OpenAI from "openai";
+import "dotenv/config";
+import puppeteer from "puppeteer-core";
+
+interface Recipe {
+  title: string;
+  ingredients: string[];
+  instructions: string[];
+}
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [recipe, setRecipe] = useState<Recipe | null>(null);
+
+  async function scraper(url: string): Promise<Recipe> {
+    const browser = await puppeteer.connect({
+      browserWSEndpoint: `wss://${process.env.BRIGHT_DATA_AUTH}@brd.superproxy.io:9222`,
+    });
+
+    const page = await browser.newPage();
+
+    await page.setViewport({ width: 1920, height: 1080 });
+
+    await page.goto(url, {
+      waitUntil: "networkidle0",
+      timeout: 30000,
+    });
+    await new Promise((r) => setTimeout(r, 1000));
+
+    // Get just the body elements from the HTML with text
+    //   const html = await page.evaluate(() => document.body.innerHTML);
+
+    const html = await page.evaluate(() => {
+      // Function to determine if an element contains recipe-like content
+
+      function isLikelyRecipeContainer(element: Element) {
+        // Extract class names, IDs, and tag name of the element
+        const classNames = element.classList;
+        const id = element.id;
+        const tagName = element.tagName.toLowerCase();
+
+        // Define keywords related to recipe content
+        const keywords = [
+          // "article",
+          // "content",
+          // "title",
+          // "ingredient",
+          // "ingredients",
+          // "instruction",
+          // "instructions",
+          // "recipe-container",
+          "wprm-recipe-name",
+          "recipe-ingredients-container",
+          "recipe-instructions-container",
+        ];
+
+        // Check if any of the keywords are found in class names, ID, or tag name
+        if (classNames.length > 0 || id !== "") {
+          // Check class names for keywords
+          for (let i = 0; i < classNames.length; i++) {
+            const className = classNames[i];
+            if (
+              keywords.some((keyword) =>
+                className.toLowerCase().includes(keyword)
+              )
+            ) {
+              return true;
+            }
+          }
+        }
+
+        // Check ID for keywords
+        if (id !== "") {
+          if (keywords.some((keyword) => id.toLowerCase().includes(keyword))) {
+            return true;
+          }
+        }
+
+        // Check tag name for keywords
+        if (keywords.includes(tagName)) {
+          return true;
+        }
+
+        return false;
+      }
+
+      const recipeElements = document.querySelectorAll("div, section, article"); // Select potential recipe containers
+      const recipeContent: string[] = [];
+
+      // Iterate over potential recipe containers
+      recipeElements.forEach((element) => {
+        // Check if the element contains recipe-like content (e.g., by analyzing its children, class names, etc.)
+        if (isLikelyRecipeContainer(element)) {
+          // If the element is identified as a recipe container, add its inner HTML to the recipe content array
+          recipeContent.push(element.innerHTML);
+        }
+      });
+
+      // Combine and return the inner HTML of all identified recipe containers
+      return recipeContent.join("");
+    });
+
+    console.log(html);
+
+    // Close the browser
+
+    await browser.close();
+
+    try {
+      const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: `Here is the HTML of a recipe website page: ${html}. Please give me the title, the ingredients, and the instructions of the recipe back in JSON format.`,
+          },
+        ],
+      });
+
+      console.log(1, response);
+      console.log(2, response.choices[0].message.content);
+
+      try {
+        const responseDataString = response.choices[0].message.content;
+
+        if (!responseDataString) {
+          throw new Error("Failed to scrape the recipe. Please try again.");
+        }
+
+        const responseData = JSON.parse(responseDataString);
+
+        const title = responseData.title;
+        const ingredients = responseData.ingredients.map((ingredient: any) => {
+          let formattedIngredient = ingredient.amount;
+          if (ingredient.unit) {
+            formattedIngredient += ` ${ingredient.unit}`;
+          }
+          formattedIngredient += ` ${ingredient.name}`;
+          if (ingredient.notes) {
+            formattedIngredient += ` (${ingredient.notes})`;
+          }
+          return formattedIngredient;
+        });
+        const instructions = responseData.instructions;
+
+        const scrapedData: Recipe = {
+          title: title,
+          ingredients: ingredients,
+          instructions: instructions,
+        };
+
+        return scrapedData;
+      } catch (error) {
+        setError("Failed to scrape the recipe. Please try again.");
+        console.log(error);
+        setLoading(false);
+        throw error;
+      }
+    } catch (error) {
+      setError("Failed to scrape the recipe. Please try again.");
+      console.log(error);
+      setLoading(false);
+      throw error;
+    }
+  }
+
+  // Get the html of the page via screenshot
+  //   const imgBase64 = await page.screenshot({
+  //     path: "screenshot.png",
+  //     fullPage: true,
+  //     encoding: "base64",
+  //   });
+
+  // Using gpt-4-turbo
+  // const response = await openai.chat.completions.create({
+  //     model: "gpt-4-turbo",
+  //     messages: [
+  //       {
+  //         role: "user",
+  //         content: [
+  //           {
+  //             type: "text",
+  //             text: "Give me the title, the ingredients, and the instructions of the recipe back in JSON format.",
+  //           },
+  //           {
+  //             type: "image_url",
+  //             image_url: {
+  //               url: "data:image/jpeg;base64," + imgBase64,
+  //               detail: "auto",
+  //             },
+  //           },
+  //         ],
+  //       },
+  //     ],
+  //   });
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget as HTMLFormElement;
+    const url = form.url.value as string;
+
+    setLoading(true);
+    setError(null);
+    await scraper(url);
+    setLoading(false);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="navbar bg-base-100 mx-10">
+        <div className="flex-1">
+          <Image
+            src={TheBearEssentialsGraphic}
+            alt="The Bear Essentials"
+            height={150}
+            width={150}
+          />
+        </div>
+        <div className="flex-none mx-10">
+          <h2 className="text-2xl mx-10">The Bear Essentials Recipe Scraper</h2>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex flex-col w-full">
+        <div className="divider mx-10"></div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      {/* // Web Scraping Form Here // */}
+      <div className="flex flex-col justify-items-center align-items">
+        <form
+          onSubmit={handleSubmit}
+          className="form-control justify-items-center mx-10"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <input
+            type="text"
+            id="url"
+            name="url"
+            placeholder="URL here"
+            className="input w-full sm:max-w-sm md:max-w-md lg:max-w-full input-bordered"
+          />
+          <label htmlFor="url"></label>
+          <button
+            type="submit"
+            disabled={loading}
+            className="btn btn-info sm:max-w-sm md:max-w-md lg:max-w-full"
+          >
+            {loading ? "Loading..." : "Scrape Recipe"}
+          </button>
+        </form>
+        {error && <p className="text-error">{error}</p>}
       </div>
-    </main>
+      <div className="flex flex-col w-full">
+        <div className="divider mx-10"></div>
+      </div>
+      {/* /* // Recipe Display Here // */}
+      {recipe && (
+        <div className="flex flex-col mx-10 my-5">
+          <h2>Recipe Title: {recipe.title}</h2>
+          <h3>Ingredients: </h3>
+          <ul>
+            {recipe.ingredients.map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+          </ul>
+
+          <h3>Instructions: </h3>
+          <ol>
+            {recipe.instructions.map((instruction, index) => (
+              <li key={index}>{instruction}</li>
+            ))}
+          </ol>
+        </div>
+      )}
+      <button className="btn btn-accent mx-10 mt-5">Print</button>
+    </>
   );
 }
